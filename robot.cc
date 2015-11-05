e98cda86ee980dc91f7f2cc9b280eb9203d01892
Shaobo Liu <sliu36@stevens.edu>
2015-11-05 18:12:19 -0500

    add robot.cc and main.cc
    LOL

---
+++ robot.cc
@@ -0,0 +1,25 @@
+#include "robot.hpp"
+
+Robot::Robot(){
+	
+}
+bool Robot::move(int num,double r,double angle){
+	
+}
+void Robot::addRobot(int x,int y,int v){
+	
+}
+bool Robot::delRobot(int num){
+	
+}
+
+int Robot::getXY(int x,int y){
+	
+}
+
+void Robot::timepass(int seconds){
+}
+
+void Robot::print(){
+	
+}
