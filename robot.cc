860e2ffb538448aea45621b6a30fee159294d561
Shaobo Liu <sliu36@stevens.edu>
2015-11-05 19:28:44 -0500

    add two functions

--- robot.cc
+++ robot.cc
@@ -1,25 +1,37 @@
 #include "robot.hpp"
+#include<cmath>
+const double pi=3.1415926;
 
 Robot::Robot(){
-	
+    
 }
 bool Robot::move(int num,double r,double angle){
-	
+    int addx=r*cos(pi*angle/180);
+    int addy=r*sin(pi*angle/180);
+    if(num!=-1) return -1;
+    else
+        return num+(addy*400+addx);
 }
+
 void Robot::addRobot(int x,int y,int v){
-	
+    
 }
 bool Robot::delRobot(int num){
-	
+    
 }
 
 int Robot::getXY(int x,int y){
-	
+    if(x<400&&y<400){
+        return y*400+x;}
+    else
+        return -1;
+    
 }
 
 void Robot::timepass(int seconds){
 }
 
 void Robot::print(){
-	
+    
 }
+
